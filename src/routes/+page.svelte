dd798f31570393db98de74b41ce1f22832f00fd4
bingual <bingual76@gmail.com>
2024-11-13 22:02:30 +0900

    수정/추가: 배포/개발 환경 분리

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,17 @@
-<script lang="ts"></script>
+<script lang="ts">
+  import type { PageData } from './$types';
+
+  let { data }: { data: PageData } = $props();
+</script>
 
 <div class="container mx-auto">
   <article class="prose lg:prose-xl">
     <h1>SVELTEKIT TEST</h1>
     <ul>
       <li><a href="/demo/paraglide">Paraglide Demo</a></li>
+      {#each data.users as user}
+        <li>{user.email}</li>
+      {/each}
     </ul>
   </article>
 </div>
