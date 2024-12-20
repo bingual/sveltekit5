132cfde33cb3f680c6c31e13ef2d94ab637b7be1
bingual <bingual76@gmail.com>
2024-12-20 15:30:26 +0900

    수정: 스크랩 코드 수정

--- generate/scraper.ts
+++ generate/scraper.ts
@@ -65,7 +65,6 @@ export const scrapManager = () => {
 
   return {
     setupBrowser,
-    sleep,
     getEmptyFieldNames,
     scrollToTheBottom,
   };
--- src/routes/playlist/melonChart/+page.server.ts
+++ src/routes/playlist/melonChart/+page.server.ts
@@ -4,6 +4,9 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
   const findPlaylist = await prisma.youTubePlaylist.findFirst({
+    orderBy: {
+      created_at: 'desc',
+    },
     include: {
       videoInfos: true,
     },
